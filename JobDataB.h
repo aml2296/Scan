95bf58c71b5e494b7faafcfcbfd01e7beacfcb1c
aml2296 <louxaaron@yahoo.com>
2022-04-19 12:00:15 -0500

    Update JobDataB.h

--- JobDataB.h
+++ JobDataB.h
@@ -1,49 +1,66 @@
+#ifndef JOBDATABASE
+#define JOBDATABASE
+
 #include <iostream>
 #include <string>
 using namespace std;
+
+class JobNode;
+class JobData;
+class JobDataB;
+
 class JobNode
 {
-    public:
-        JobNode *first = nullptr;
-        JobNode *last = nullptr;
-        JobNode *next = nullptr;
-        JobNode *prev = nullptr;
-        JobData *data = nullptr;
-    
-        int GetID();
-        string StringID();
-        string Name();
-        string ToString();
-    
-        JobNode();
-        JobNode(string nameF, string nameL, int _id,  JobNode *firstPtr = nullptr, JobNode *lastPtr = nullptr, JobNode *nextPtr = nullptr, JobNode *prevPtr = nullptr);
+public:
+  JobNode * first = nullptr;
+  JobNode *last = nullptr;
+  JobNode *next = nullptr;
+  JobNode *prev = nullptr;
+  JobData *data = nullptr;
+
+    JobNode ();
 };
 class JobData
 {
-        private:
-        string firstName;
-        string lastName;
-        int JobID = -1;
+private:
+  string firstName;
+  string lastName;
+  int JobID = -1;
 
+public:
+  int GetID ();
+  string StringID ();
+  string Name ();
+  string ToString ();
 
-        int GetID();
-        string StringID();
-        string Name();
-        string ToString();
-}
+    JobData ();
+    JobData (string nameF, string nameL, int _id, JobNode * firstPtr =
+	     nullptr, JobNode * lastPtr = nullptr, JobNode * nextPtr =
+	     nullptr, JobNode * prevPtr = nullptr);
+};
 class JobDataB
 {
-    private:
-    JobNode *jobHead = nullptr;
-    JobNode *jobTail = nullptr;
+private:
+  JobNode *jobHead = nullptr;
+  JobNode *jobTail = nullptr;
+  JobNode *jobCurrent = nullptr;
+  int count = 0;
 
-    int count = 0;
-    
-    public: 
-    JobDataB()
-    {
-        
-    }
-    string ToString();
-    bool AddJobData(string nameF, string nameL, int _id);
+  bool InsertJobNode (JobNode *);
+
+
+public:
+  JobDataB ();
+  int Count();
+  string ToString ();
+  bool SetIterator(int);
+  JobData Head();
+  JobData Tail();
+  JobData Current();
+  JobData Prev();
+  JobData Next();
+  bool DeleteJob (int);
+  bool AddEmptyData ();
+  bool AddJobData (string nameF, string nameL, int _id);
 };
+#endif
