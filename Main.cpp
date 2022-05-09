6b51c5e0130a08552e73e2f4ffd8daab608f7431
aml2296 <louxaaron@yahoo.com>
2022-05-09 10:07:01 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,22 @@
 #include "JobData.cpp"
 #include "BMat.cpp"
 #include "FManager.cpp"
+void Write(JobDataB&,FManager&);
+
 int main()
 {
     JobDataB temp;
+    FManager fMang(temp);
+    fMang.ReadData();
+    return 0;
+}
+
+void Write(JobDataB &temp, FManager &fMang)
+{
     temp.AddJobData("Tim","M", 6969);
     temp.AddJobData("Abe", "A",80085 );
     temp.AddJobData("Christain", "B", 666);
     temp.AddJobData("British", "Guy", 3434);
     temp.AddJobData("Fuck","Shit",999900);
-    FManager fMang(temp);
     fMang.WriteData();
-    std::cout << temp.ToString();
-    return 0;
 }
