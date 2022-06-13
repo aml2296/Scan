97e613726ddafdab7dbc4f889de96b776a4576e0
aml2296 <louxaaron@yahoo.com>
2022-06-13 10:33:27 -0500

    Update JobDataB.cpp

--- JobDataB.cpp
+++ JobDataB.cpp
@@ -2,290 +2,354 @@
 using namespace std;
 
 
-inline int JobData::GetID()
+inline int
+JobData::GetID ()
 {
-    return JobID;
+  return JobID;
 }
-inline string JobData::StringID()
+
+inline string
+JobData::StringID ()
 {
-    return to_string(JobID);
+  return to_string (JobID);
 }
-inline string JobData::Name()
+
+inline string
+JobData::Name ()
 {
-    string rStr = "";
-    rStr += firstName;
-    rStr += " ";
-    rStr += lastName;
-    return rStr.c_str();
+  string rStr = "";
+  rStr += firstName;
+  rStr += " ";
+  rStr += lastName;
+  return rStr.c_str ();
 }
-inline string JobData::ToString()
+
+inline string
+JobData::ToString ()
 {
-    string str = StringID();
-    str += " ";
-    str += Name();
-    return str;
+  string str = StringID ();
+  str += " ";
+  str += Name ();
+  return str;
 }
-inline string JobData::Compress()
+
+inline string
+JobData::Compress ()
 {
-    string str = "";
-    str += StringID();
-    str += DataBreak; //Record Seperator
-    str += firstName;
-    str += DataBreak;
-    str += lastName;
-    return str;
+  string str = "";
+  str += StringID ();
+  str += DataBreak;		//Record Seperator
+  str += firstName;
+  str += DataBreak;
+  str += lastName;
+  return str;
 }
-inline JobNode::JobNode()
+
+inline
+JobNode::JobNode ()
 {
-    
+
 }
-inline JobData::JobData()
+
+inline
+JobData::JobData ()
 {
- firstName = "No";
- lastName = "Name";
- JobID = -1;
+  firstName = "No";
+  lastName = "Name";
+  JobID = -1;
 }
-inline JobData::JobData(string nameF, string nameL, int _id,JobNode *firstPtr, JobNode *lastPtr, JobNode *nextPtr, JobNode *prevPtr)
+
+inline
+JobData::JobData (string nameF, string nameL, int _id, JobNode * firstPtr,
+		  JobNode * lastPtr, JobNode * nextPtr, JobNode * prevPtr)
 {
-    firstName = nameF;
-    lastName = nameL;
-    
-    JobID = _id < 0? 0 : _id;
+  firstName = nameF;
+  lastName = nameL;
+
+  JobID = _id < 0 ? 0 : _id;
 }
-inline JobData::JobData(string dataStr)
+
+inline
+JobData::JobData (string dataStr)
 {
-    int iterator = 0;
-    int endDataBlock = 0;
-    int startDataBlock = iterator;
-    JobData returnValue;
-    
-    while(dataStr.length() > 0)
+  int iterator = 0;
+  int endDataBlock = 0;
+  int startDataBlock = iterator;
+  JobData returnValue;
+
+  while (dataStr.length () > 0)
     {
-        int subStrLength = dataStr.find(DataBreak,0);
-        string subStr = dataStr.subStr(0,subStrLength - 1);
-        returnValue.ReadID(substr);
-        
-        dataStr.erase(0,subStrLength);
+      int subStrLength = dataStr.find (DataBreak, 0);
+      string subStr = dataStr.substr (0, subStrLength - 1);
+      returnValue.ReadID (subStr);
+
+      dataStr.erase (0, subStrLength);
     }
-    
-    
-    
-    
-    
-    while(iterator < dataStr.length() 
-    && dataStr[iterator] != EndOfData)
+
+
+
+
+
+  while (iterator < dataStr.length () && dataStr[iterator] != EndOfData)
     {
-        while(dataStr[iterator] != DataBreak
-        && dataSTR[iterator] != EndOfData)
-        {
-            
-            iterator++;
-        }
-        iterator++;
+      while (dataStr[iterator] != DataBreak && dataStr[iterator] != EndOfData)
+	{
+
+	  iterator++;
+	}
+      iterator++;
     }
 }
-inline int JobDataB::Count()
+
+inline void
+JobData::ReadID (string strID)
 {
- return count;   
+  stringstream ss (strID);
+  ss >> JobID;
+  std::cout << JobID;
 }
-inline JobDataB::JobDataB()
+
+inline int
+JobDataB::Count ()
 {
-    count = 0;
+  return count;
 }
-inline ReadID(string strID)
+
+inline
+JobDataB::JobDataB ()
 {
-    stringstream ss(strID);
-    ss >> JobID;
-    std::cout << JobID;
+  count = 0;
 }
-inline string JobDataB::Compress()
+
+inline string
+JobDataB::Compress ()
 {
-        JobNode* iterator = jobHead;
-        std::string str = "";
-        std::cout<< count;
-        for(int i =0; i < count && iterator != nullptr; i++,iterator =  iterator->next)
-        {
-            str += iterator->data->Compress();
-            str += JobData::EndOfData; //Char to represent end of record
-        }
-        return str == ""?"No Job's in database!!" : str;
+  JobNode *iterator = jobHead;
+  std::string str = "";
+  std::cout << count;
+  for (int i = 0; i < count && iterator != nullptr;
+       i++, iterator = iterator->next)
+    {
+      str += iterator->data->Compress ();
+      str += JobData::EndOfData;	//Char to represent end of record
+    }
+  return str == "" ? "No Job's in database!!" : str;
 }
-inline string JobDataB::ToString()
+
+inline string
+JobDataB::ToString ()
+{
+  JobNode *iterator = jobHead;
+  std::string str = "";
+  std::cout << count;
+  for (int i = 0; i < count && iterator != nullptr;
+       i++, iterator = iterator->next)
     {
-        JobNode* iterator = jobHead;
-        std::string str = "";
-        std::cout<< count;
-        for(int i =0; i < count && iterator != nullptr; i++,iterator =  iterator->next)
-        {
-            str += iterator->data->ToString();
-            str += '\n';
-        }
-        return str == ""?"No Job's in database!!" : str;
+      str += iterator->data->ToString ();
+      str += '\n';
     }
-    inline bool JobDataB::AddEmptyData()
+  return str == "" ? "No Job's in database!!" : str;
+}
+
+inline bool
+JobDataB::AddEmptyData ()
+{
+  JobNode *nData = new JobNode ();
+  nData->data = new JobData ();
+  return InsertJobNode (nData);
+}
+
+inline bool
+JobDataB::AddJobData (string nameF, string nameL, int _id)
+{
+  JobNode *nData = new JobNode ();
+  nData->data = new JobData (nameF, nameL, _id);
+  return InsertJobNode (nData);
+}
+
+inline void
+JobDataB::Clear ()
+{
+  if (jobHead != nullptr)
     {
-        JobNode* nData = new JobNode();
-        nData->data = new JobData();
-        return InsertJobNode(nData);
+      JobNode *temp = jobHead;
+      while (temp != nullptr)
+	{
+	  JobNode *next = temp->next;
+	  delete temp->data;
+	  delete temp;
+	  temp = next;
+	}
     }
-    inline bool JobDataB::AddJobData(string nameF, string nameL, int _id)
+}
+
+inline bool
+JobDataB::DeleteJob (int _id)
+{
+  //Search for Job
+  if (_id < jobTail->data->GetID ())
+    return false;
+  JobNode *iterator = jobHead;
+  while (iterator != nullptr && iterator->data->GetID () > _id)
+    iterator = iterator->next;
+  if (iterator == nullptr)
+    return false;
+  //RemoveJob
+  if (iterator->next != nullptr)
     {
-        JobNode* nData = new JobNode();
-        nData->data = new JobData(nameF,nameL,_id);
-        return InsertJobNode(nData);
+      iterator->next->prev = iterator->prev;
     }
-    inline bool JobDataB::DeleteJob(int _id)
+  else
     {
-        //Search for Job
-        if(_id < jobTail->data->GetID())
-            return false;
-        JobNode* iterator = jobHead;
-        while(iterator != nullptr && iterator->data->GetID() > _id)
-            iterator = iterator->next;
-        if(iterator == nullptr)
-            return false;
-        //RemoveJob
-        if(iterator->next != nullptr)
-        {
-            iterator->next->prev = iterator->prev;
-        }
-        else
-        {
-            jobTail = iterator->prev;
-        }
-        if(iterator->prev != nullptr)
-        {
-            iterator->prev->next = iterator->next;
-        }
-        else
-        {
-            jobHead = iterator->next;
-        }
-        delete iterator->data;
-        delete iterator;
-        count--;
-        return true;
+      jobTail = iterator->prev;
     }
-    inline bool JobDataB::InsertJobNode(JobNode *job)
+  if (iterator->prev != nullptr)
     {
-        if(job == nullptr)
-            return false;
-        if(count == 0)
-        {
-          jobCurrent =  jobTail = jobHead = job;
-        }
-        else if(count > 0)
-        {
-            if(count == 1)
-            {
-                if(jobHead->data->GetID() >= job->data->GetID())
-                {
-                    jobCurrent = jobTail = job;
-                }
-                else
-                {
-                    jobCurrent = jobHead = job;
-                }
-                    jobTail->prev = jobHead;
-                    jobHead->next = jobTail;
-            }
-            else
-            {
-                JobNode IT;
-                JobNode *iterator = jobHead;
-                
-                    if(jobHead->data->GetID() < job->data->GetID())
-                    {
-                         jobHead->prev = job;
-                         job->next = jobHead;
-                         jobHead = job;
-                    }
-                    else if(jobTail->data->GetID() <= job->data->GetID())
-                    {
-                        jobTail->next = job;
-                        job->prev = jobTail;
-                        jobTail = job;
-                    }
-                    else
-                    {
-                    while(iterator != nullptr && iterator->data->GetID() >= job->data->GetID())
-                    {
-                        IT = *iterator;
-                        iterator = iterator->next;
-                    }
-                    if(iterator == nullptr)
-                    {
-                        jobTail->next = job;
-                        job->prev = jobTail;
-                        jobCurrent = jobTail = job;
-                    }
-                    else
-                    {
-                        iterator->prev->next = job;
-                        job->prev = iterator->prev;
-                        iterator->prev = job;
-                        job->next = iterator;
-                        jobCurrent = job;
-                    }
-                }
-            }
-        }
-        else
-        {
-            return false;
-        }
-        count++;
-    return true;
+      iterator->prev->next = iterator->next;
+    }
+  else
+    {
+      jobHead = iterator->next;
+    }
+  delete iterator->data;
+  delete iterator;
+  count--;
+  return true;
+}
+
+inline bool
+JobDataB::InsertJobNode (JobNode * job)
+{
+  if (job == nullptr)
+    return false;
+  if (count == 0)
+    {
+      jobCurrent = jobTail = jobHead = job;
+    }
+  else if (count > 0)
+    {
+      if (count == 1)
+	{
+	  if (jobHead->data->GetID () >= job->data->GetID ())
+	    {
+	      jobCurrent = jobTail = job;
+	    }
+	  else
+	    {
+	      jobCurrent = jobHead = job;
+	    }
+	  jobTail->prev = jobHead;
+	  jobHead->next = jobTail;
+	}
+      else
+	{
+	  JobNode IT;
+	  JobNode *iterator = jobHead;
+
+	  if (jobHead->data->GetID () < job->data->GetID ())
+	    {
+	      jobHead->prev = job;
+	      job->next = jobHead;
+	      jobHead = job;
+	    }
+	  else if (jobTail->data->GetID () <= job->data->GetID ())
+	    {
+	      jobTail->next = job;
+	      job->prev = jobTail;
+	      jobTail = job;
+	    }
+	  else
+	    {
+	      while (iterator != nullptr
+		     && iterator->data->GetID () >= job->data->GetID ())
+		{
+		  IT = *iterator;
+		  iterator = iterator->next;
+		}
+	      if (iterator == nullptr)
+		{
+		  jobTail->next = job;
+		  job->prev = jobTail;
+		  jobCurrent = jobTail = job;
+		}
+	      else
+		{
+		  iterator->prev->next = job;
+		  job->prev = iterator->prev;
+		  iterator->prev = job;
+		  job->next = iterator;
+		  jobCurrent = job;
+		}
+	    }
+	}
+    }
+  else
+    {
+      return false;
+    }
+  count++;
+  return true;
 }
 
 
-inline bool JobDataB::SetIterator(int pos)
+inline bool
+JobDataB::SetIterator (int pos)
 {
-    if(count < pos)
-        return false;
-    else
+  if (count < pos)
+    return false;
+  else
     {
-        jobCurrent = jobHead;
-        for(int i =0; i < pos && jobCurrent != nullptr; i++)
-            Next();
-        if(jobCurrent == nullptr)
-            return false;
+      jobCurrent = jobHead;
+      for (int i = 0; i < pos && jobCurrent != nullptr; i++)
+	Next ();
+      if (jobCurrent == nullptr)
+	return false;
     }
-    return true;
+  return true;
 };
-inline JobData JobDataB::Head()
+
+inline JobData
+JobDataB::Head ()
 {
-    if(jobHead != nullptr)
-        return *jobHead->data;
-    return JobData();
+  if (jobHead != nullptr)
+    return *jobHead->data;
+  return JobData ();
 }
-inline JobData JobDataB::Tail()
+
+inline JobData
+JobDataB::Tail ()
 {
-    if(jobTail != nullptr)
-        return *jobTail->data;
-    return JobData();
+  if (jobTail != nullptr)
+    return *jobTail->data;
+  return JobData ();
 }
-inline JobData JobDataB::Current()
+
+inline JobData
+JobDataB::Current ()
 {
-    if(jobCurrent != nullptr)
-        return *jobCurrent->data;
-    return JobData();
+  if (jobCurrent != nullptr)
+    return *jobCurrent->data;
+  return JobData ();
 }
-inline JobData JobDataB::Next()
+
+inline JobData
+JobDataB::Next ()
 {
-    if(jobCurrent != nullptr && jobCurrent->next != nullptr)
+  if (jobCurrent != nullptr && jobCurrent->next != nullptr)
     {
-        jobCurrent = jobCurrent->next;
-        return *jobCurrent->data;
+      jobCurrent = jobCurrent->next;
+      return *jobCurrent->data;
     }
-    return JobData();
+  return JobData ();
 }
-inline JobData JobDataB::Prev()
+
+inline JobData
+JobDataB::Prev ()
 {
-    if(jobCurrent != nullptr && jobCurrent->prev != nullptr)
+  if (jobCurrent != nullptr && jobCurrent->prev != nullptr)
     {
-        jobCurrent = jobCurrent->prev;
-        return *jobCurrent->data;
+      jobCurrent = jobCurrent->prev;
+      return *jobCurrent->data;
     }
-    return JobData();
+  return JobData ();
 }
